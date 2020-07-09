8913eb7066c8fc8006e35cccb3c5b2487d7aefd1
Luiz Begalli <luizmartins.begalli@gmail.com>
2020-07-09 09:05:31 -0300

    Projeto iniciado, mostrando Hello Worldgit status

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
